e811131c8dd544c061d8b2c33380b80bc8d049b6
nikolas-helling <helling.nikolas@gmail.com>
2024-12-23 12:07:32 +0100

    Modify style

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -3,12 +3,10 @@
 // Toggle through light, dark, and system theme settings.
 let toggleThemeSetting = () => {
   let themeSetting = determineThemeSetting();
-  if (themeSetting == "system") {
+  if (themeSetting == "dark") {
     setThemeSetting("light");
-  } else if (themeSetting == "light") {
-    setThemeSetting("dark");
   } else {
-    setThemeSetting("system");
+    setThemeSetting("dark");
   }
 };
 
